436a4e0c5baab988619cb7bcd3d3618a6894da07
Nick Nguyen <khangnick14@gmail.com>
2024-10-14 23:23:43 +0700

    WIP: add custom annotation with flag + aspect code

--- src/main/java/com/leon/baobui/resilience4j/annotation/Resilience4jCustom.java
+++ src/main/java/com/leon/baobui/resilience4j/annotation/Resilience4jCustom.java
@@ -1,34 +1,23 @@
 package main.java.com.leon.baobui.resilience4j.annotation;
 
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
-import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
-import org.springframework.core.annotation.AliasFor;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-@Documented
+@Target(ElementType.METHOD)
 public @interface Resilience4jCustom {
 
   boolean enableCircuitBreaker() default true;
-  boolean enableRetry() default true;
-  boolean enableBulkhead() default true;
-  boolean enableTimeLimiter() default true;
-
-  @AliasFor(annotation = CircuitBreaker.class, attribute = "name")
   String circuitBreakerName() default "defaultCircuitBreaker";
 
-  @AliasFor(annotation = Retry.class, attribute = "name")
+  boolean enableRetry() default true;
   String retryName() default "defaultRetry";
 
-  @AliasFor(annotation = Bulkhead.class, attribute = "name")
-  String bulkheadName() default "defaultBulkhead";
+  boolean enableRateLimiter() default false;
+  String rateLimiterName() default "defaultRateLimiter";
 
-  @AliasFor(annotation = TimeLimiter.class, attribute = "name")
-  String timeLimiterName() default "defaultTimeLimiter";
-
-  String fallbackMethod() default "fallback";
+  boolean enableBulkhead() default false;
+  String bulkheadName() default "defaultThreadPoolBulkhead";
 }
--- src/main/java/com/leon/baobui/resilience4j/aop/ResilienceAspect.java
+++ src/main/java/com/leon/baobui/resilience4j/aop/ResilienceAspect.java
@@ -1,10 +1,59 @@
 package main.java.com.leon.baobui.resilience4j.aop;
 
+import io.github.resilience4j.bulkhead.Bulkhead;
+import io.github.resilience4j.bulkhead.ThreadPoolBulkhead;
+import io.github.resilience4j.bulkhead.ThreadPoolBulkheadRegistry;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.core.functions.CheckedSupplier;
+import io.github.resilience4j.decorators.Decorators;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryRegistry;
+import lombok.RequiredArgsConstructor;
+import main.java.com.leon.baobui.resilience4j.annotation.Resilience4jCustom;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import
 
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class ResilienceAspect {
+  private final CircuitBreakerRegistry circuitBreakerRegistry;
+  private final RetryRegistry retryRegistry;
+  private final RateLimiterRegistry rateLimiterRegistry;
+  private final ThreadPoolBulkheadRegistry threadPoolBulkheadRegistry;
 
+  @Around("@annotation(resilience4jCustom)")
+  public Object applyResilience(ProceedingJoinPoint joinPoint, Resilience4jCustom resilience4jCustom) throws Throwable {
+    CheckedSupplier<Object> supplier = joinPoint::proceed;
+    Decorators.DecorateCheckedSupplier<Object> decoratedSupplier = Decorators.ofCheckedSupplier(supplier);
+
+
+    if (resilience4jCustom.enableCircuitBreaker()) {
+      CircuitBreaker circuitBreaker = circuitBreakerRegistry.circuitBreaker(resilience4jCustom.circuitBreakerName());
+      decoratedSupplier.withCircuitBreaker(circuitBreaker);
+    }
+
+    if (resilience4jCustom.enableRetry()) {
+      Retry retry = retryRegistry.retry(resilience4jCustom.retryName());
+      decoratedSupplier.withRetry(retry);
+    }
+
+    if (resilience4jCustom.enableRateLimiter()) {
+      RateLimiter rateLimiter = rateLimiterRegistry.rateLimiter(resilience4jCustom.rateLimiterName());
+      decoratedSupplier.withRateLimiter(rateLimiter);
+    }
+
+//    if (resilience4jCustom.enableBulkhead()) {
+//      ThreadPoolBulkhead bulkhead = threadPoolBulkheadRegistry.bulkhead(resilience4jCustom.bulkheadName());
+//      supplier = ThreadPoolBulkhead.decorateSupplier(bulkhead, supplier);
+//    }
+
+    return decoratedSupplier.get();
+  }
 }
